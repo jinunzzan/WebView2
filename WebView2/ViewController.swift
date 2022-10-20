e8b713a98d942a06d6329745ba2c61d2b06b8146
jinunzzan <lovebox6607@gmail.com>
2022-10-21 03:30:56 +0900

    Initial Commit

---
+++ WebView2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WebView2
+//
+//  Created by Eunchan Kim on 2022/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
