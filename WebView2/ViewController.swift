cdce915edf0d27782e4d55c49e13ff9431d24a24
jinunzzan <lovebox6607@gmail.com>
2022-10-21 03:51:53 +0900

    webView2 여러개 사이트 이동 만들어보기

--- WebView2/ViewController.swift
+++ WebView2/ViewController.swift
@@ -6,14 +6,42 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var webView: WKWebView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        if let url = URL(string: "http://daum.net"){
+            let request = URLRequest(url: url)
+            webView.load(request)
+        }
     }
 
-
+    
+    
+    @IBAction func goGoogle(_ sender: Any) {
+        if let google = URL(string: "http://www.google.com"){
+            let request = URLRequest(url: google)
+            webView.load(request)
+        }
+    }
+    
+    
+    @IBAction func goNaver(_ sender: Any) {
+        if let naver = URL(string: "http://www.naver.com"){
+            let request = URLRequest(url: naver)
+            webView.load(request)
+        }
+    }
+    
+    @IBAction func goDaum(_ sender: Any) {
+        if let daum = URL(string: "http://www.daum.net"){
+            let request = URLRequest(url: daum)
+            webView.load(request)
+        }
+    }
 }
-
